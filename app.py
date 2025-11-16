503f20e093b918c1761482ddc86ad53904de8379
Gangadhar Tiwari <115203727+gangadhartiwari@users.noreply.github.com>
2025-11-16 18:54:31 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -907,5 +907,6 @@ if __name__ == '__main__':
     else:
         print("Database exists → Skipping database initialization.")
 
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
